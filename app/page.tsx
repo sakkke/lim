1660e0d431164647d7c49db3d20a1ecc90e54597
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-09-30 12:14:04 +0900

    feat(manifest.json): add short_name, name, start_url, display, and theme_color to enhance PWA capabilities feat(page.tsx): register service worker to enable offline functionality and improve performance feat(sw.js): create a basic service worker to handle fetch events for caching and offline support

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import { MapboxAppComponent } from "@/components/mapbox-app";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+    }
+  }, [])
+
   return (
     <MapboxAppComponent />
   );
