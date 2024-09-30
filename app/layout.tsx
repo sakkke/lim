e417cc7299590943fad28d1fb2904c45e2f54272
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-09-30 12:13:18 +0900

    feat(layout.tsx): add Head component to include favicon and apple touch icon for better branding feat(manifest.json): create manifest file to define app icons for various sizes and purposes feat(public): add favicon and app icons to enhance user experience and branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
